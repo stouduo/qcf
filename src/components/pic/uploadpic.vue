19e603113b4d0683baf7c01eaf94327ea41eea0a
stouduo <stouduo@qq.com>
2018-01-25 10:05:21 +0800

    6

--- src/components/pic/uploadpic.vue
+++ src/components/pic/uploadpic.vue
@@ -7,17 +7,17 @@
       <div class="weui-uploader__bd">
         <ul class="weui-uploader__files" id="uploaderFiles">
           <li class="weui-uploader__file weui-uploader__file_status" v-for="(pic,index) in files"
-              :style="{backgroundImage:'url(pic.url)'}">
+              :style="{backgroundImage:'url('+pic.url+')'}">
             <div class="weui-uploader__file-content">
-              <p v-if="pic.unstart">未开始</p>
-              <p v-if="pic.process">50%</p>
-              <i v-if="pic.success" class="weui-icon-warn"></i>
-              <i v-if="pic.error" class="weui-icon-success"></i>
+              <p v-if="pic.status=='unstart'">未开始</p>
+              <p v-if="pic.status=='process'" :text="pic.process"></p>
+              <p v-if="pic.status=='success'">上传成功</p>
+              <p v-if="pic.status=='error'" style="cursor:pointer" title="点击重传" @click="reupload(index)">上传失败</p>
             </div>
           </li>
         </ul>
         <div class="weui-uploader__input-box">
-          <input id="uploaderInput" class="weui-uploader__input" @chage="autoUpload" type="file" accept="image/*"
+          <input id="uploaderInput" class="weui-uploader__input" @change="autoUpload" type="file" accept="image/*"
                  multiple/>
         </div>
       </div>
@@ -35,6 +35,21 @@
       }
     },
     methods: {
+      reupload: function (index) {
+        let pic = this.files[index];
+        uploadPic(pic.file, (e) => {
+          if (e.lengthComputable) {
+            pic.status = 'process';
+            pic.process = Math.ceil(e.loaded / e.total);
+          }
+        }).then(() => {
+          pic.status = 'success';
+          pic.process = 0;
+        }).catch(() => {
+          pic.status = 'error';
+          pic.process = 0;
+        });
+      },
       autoUpload: function (e) {
         let pics = e.target.files;
         let i = 0;
@@ -45,34 +60,29 @@
         reader.onload = function () {
           pic = {
             url: reader.result,
-            unstart: true,
-            error: false,
-            success: false,
+            status: 'unstart',
             process: 0,
             file: pics[i]
           };
           _this.files.push(pic);
           uploadPic(pics[i], (e) => {
             if (e.lengthComputable) {
-              pic.unstart = false;
-              pic.success = true;
-              pic.error = false;
+              pic.status = 'process';
               pic.process = Math.ceil(e.loaded / e.total);
             }
-          }).then(res => {
-            pic.unstart = false;
-            pic.success = true;
-            pic.error = false;
+          }).then(() => {
+            if (++i < pics.length) {
+              reader.readAsDataURL(pics[i]);
+            }
+            pic.status = 'success';
             pic.process = 0;
-          }).catch((err) => {
-            pic.unstart = false;
-            pic.success = false;
-            pic.error = true;
+          }).catch(() => {
+            if (++i < pics.length) {
+              reader.readAsDataURL(pics[i]);
+            }
+            pic.status = 'error';
             pic.process = 0;
           });
-          if (i < pics.length) {
-            reader.readAsDataURL(pics[++i]);
-          }
         }
       }
     }
